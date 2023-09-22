9ac8822c0af030364b2a226ce172ddb4bc858bed
andreia-negreira <andreia_heringer@hotmail.com>
2023-09-22 14:42:30 +0200

    ready

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# becode_check.in.out_button_automatization
+# Automatised "push the button" code
+
+## Usage
+
+### Important
+
+- If you follow those steps, keep in mind that your code will be running locally. It means that your terminal will be open showing the task running and it relays on your computer to be turned on and not be in sleep mode.
+
+- In case you just want to run it locally, but is not interested on your terminal displaying the log, take a look at the method BackgroundScheduler (apscheduler).
+
+- You can also store and run this code in a EC2 instance, feel free.
+
+1. Install all the requirements
+2. Change the becode token
+3. Run the code
+
+Voilà.
---
+++ becode_automation.py
@@ -0,0 +1,101 @@
+import requests
+from enum import Enum
+from datetime import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# Get your personal token on: 
+# mybecode --> inspect --> network --> 
+# fetch/xhr --> name (graph.becode.org) --> headers (request headers) --> authorization
+
+# IMPORTANT: you will be needing to check the type of the request! 
+# To do so, instad of headers, go to payloads and check if the json matchs the payload of the functions bellow
+
+class AttendanceTimePeriod(Enum):
+    Morning = 'MORNING'
+    Lunch = 'LUNCH'
+    Noon = 'NOON'
+    Evening = 'EVENING'
+
+# Getting day object for the junior_attendance request
+def day_object():
+    return datetime.timestamp(datetime.now())
+
+# Request that pass the object of the day, hour, minute and second to becode graph regarding the time frame which the button was pushed
+def get_junior_today_attendance():
+    # Change your personal token for this request here (remember to verify the payload!)
+    junior_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoib3duZXIiLCJ1aWQiOiI2ZDI4NmYwNy0wMzMzLTQyYTItOTNjNy1hZjhhYzNlNzg1OGYiLCJrZXkiOiI0ZGI1MmI3NyIsImlhdCI6MTY5NTExMTA5OH0.ZSdvZwHKYCA29yg3yEQTWtHqvQyYJNFHhvODRhdkF_s'
+    junior_payload = {
+    "operationName": "get_junior_today_attendance", 
+     "variables": {"day": day_object()}, 
+     "extensions": {
+        "persistedQuery": {
+            "version": 1, 
+            "sha256Hash": "2d903b4f72e5ce35ab4239a53e48612aec6e5469eaeb76de603b68bac7f6410c"
+            }
+        }
+     }
+    resp = requests.post(
+    'https://graph.becode.org/',
+    json=junior_payload,
+    headers={'Authorization': f'Bearer {junior_token}'}
+    )
+
+# Request that "push the hour button". It passes the request with a personal token and the status (bool) of where the student is
+def record_attendance(bool):
+    at_home = bool
+    hour = datetime.now().hour
+    minute = datetime.now().minute
+    print(f'Running record_attendace at {hour}:{minute}')
+    if hour == 9:
+        time_period = AttendanceTimePeriod.Morning
+        # When calling this function here, the first request is sent and then the scheduler is going to trigger and start job for the current request right after
+        get_junior_today_attendance()
+    elif hour == 12:
+        time_period = AttendanceTimePeriod.Lunch
+    elif hour == 13:
+        time_period = AttendanceTimePeriod.Noon
+    elif hour == 17:
+        time_period = AttendanceTimePeriod.Evening
+    else:
+        return None
+
+    # Change your personal token for this request here (remember to verify the payload!)
+    becode_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoib3duZXIiLCJ1aWQiOiI2ZDI4NmYwNy0wMzMzLTQyYTItOTNjNy1hZjhhYzNlNzg1OGYiLCJrZXkiOiI0ZGI1MmI3NyIsImlhdCI6MTY5NTExMTA5OH0.ZSdvZwHKYCA29yg3yEQTWtHqvQyYJNFHhvODRhdkF_s'
+    becode_payload = {
+        "operationName": "record_attendance_time",
+        "variables": {"period": time_period.value, "atHome": at_home},
+        "extensions": {
+            "persistedQuery": {
+                "version": 1,
+                "sha256Hash": "553ae433516c13a97e348d4a48dd0114d1949f791ab21d97bed27030a65e85a8",
+            }
+        },
+    }
+
+    resp = requests.post(
+        'https://graph.becode.org/',
+        json=becode_payload,
+        headers={'Authorization': f'Bearer {becode_token}'}
+    )
+    
+    print(f'Status Code request -> {resp.status_code}')
+    print(f'Request response -> {resp.json()}')
+
+# Instantiating an object with the scheduler
+scheduler = BlockingScheduler()
+
+# Jobs order
+# It is scheduled accordingly to the days that the student is at home or at campus
+scheduler.add_job(record_attendance, 'cron', day_of_week='mon,tue,thu', hour='9,17', args=[False])
+scheduler.add_job(record_attendance, 'cron', day_of_week='mon,tue,thu', hour='12,13', minute=30, args=[False])
+scheduler.add_job(record_attendance, 'cron', day_of_week='wed,fri', hour='9,17', args=[True])
+scheduler.add_job(record_attendance, 'cron', day_of_week='wed,fri', hour='12,13', minute=30, args=[True])
+
+# scheduler trigger
+# it is going to be executed on the terminal
+# it is possible to change it to be executed on the background (see object BackgroundScheduler)
+try:
+    scheduler.start()
+except KeyboardInterrupt:
+    # Ctrl+C to interrupt the job
+    pass
