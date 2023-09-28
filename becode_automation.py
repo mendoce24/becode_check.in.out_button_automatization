391ce7d77a5a86286c005495dbafb7a267e06e13
Mykola Senko <mykola-senko@becode.xyz>
2023-09-28 14:17:48 +0200

    added cridentials

--- becode_automation.py
+++ becode_automation.py
@@ -7,8 +7,7 @@ import os
 # mybecode --> inspect --> network --> 
 # fetch/xhr --> name (graph.becode.org) --> headers (request headers) --> authorization
 
-# IMPORTANT: you will be needing to check the type of the request! 
-# To do so, instead of headers, go to payloads and check if the json matches the payload of the functions bellow
+
 
 class AttendanceTimePeriod(Enum):
     Morning = 'MORNING'
@@ -21,9 +20,8 @@ def day_object():
     return datetime.timestamp(datetime.now())
 
 # Request that pass the object of the day, hour, minute and second to becode graph regarding the time frame which the button was pushed
-def get_junior_today_attendance():
-    # Change your personal token for this request here (remember to verify the payload!)
-    junior_token = 'CRED'
+def get_junior_today_attendance(token):
+    
     junior_payload = {
     "operationName": "get_junior_today_attendance", 
      "variables": {"day": day_object()}, 
@@ -37,18 +35,18 @@ def get_junior_today_attendance():
     resp = requests.post(
     'https://graph.becode.org/',
     json=junior_payload,
-    headers={'Authorization': f'Bearer {junior_token}'}
+    headers={'Authorization': f'Bearer {token}'}
     )
 
 # Request that "push the hour button". It passes the request with a personal token and the status (bool) of where the student is
-def record_attendance(at_home):
+def record_attendance(at_home, token):
     hour = datetime.now().hour
     minute = datetime.now().minute
     print(f'Running record_attendace at {hour}:{minute}')
     if hour == 9:
         time_period = AttendanceTimePeriod.Morning
         # When calling this function here, the first request is sent and then the scheduler is going to trigger and start job for the current request right after
-        get_junior_today_attendance()
+        get_junior_today_attendance(token)
     elif hour == 12:
         time_period = AttendanceTimePeriod.Lunch
     elif hour == 13:
@@ -58,8 +56,7 @@ def record_attendance(at_home):
     else:
         return None
 
-    # Change your personal token for this request here (remember to verify the payload!)
-    becode_token = 'CRED'
+    
     becode_payload = {
         "operationName": "record_attendance_time",
         "variables": {"period": time_period.value, "atHome": at_home},
@@ -74,7 +71,7 @@ def record_attendance(at_home):
     resp = requests.post(
         'https://graph.becode.org/',
         json=becode_payload,
-        headers={'Authorization': f'Bearer {becode_token}'}
+        headers={'Authorization': f'Bearer {token}'}
     )
     
     print(f'Status Code request -> {resp.status_code}')
@@ -83,7 +80,8 @@ def record_attendance(at_home):
 def main():
     # Access the environment variable and convert it to a boolean
     at_home = os.environ.get("AT_HOME", "").lower() == "true"
-    record_attendance(at_home)
+    token = os.environ.get("TOKEN")
+    record_attendance(at_home, token)
 
 if __name__ == "__main__":
     main()
